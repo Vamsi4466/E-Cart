4f08f36e53da8316b5b643395729df2e95641b45
Vamsi4466 <settyvamsikrishna83@gmail.com>
2024-03-09 12:19:40 +0530

    first commit

---
+++ src/utils/images.js
@@ -0,0 +1,9 @@
+import slider1_img_1 from "../assets/images/slider1-img-1.png";
+import slider1_img_2 from "../assets/images/slider1-img-2.png";
+import shopping_cart from "../assets/images/shopping_cart.png";
+import correct from "../assets/images/correct.png";
+import loader from "../assets/images/loader.svg";
+ 
+
+const sliderImgs = [slider1_img_1, slider1_img_2];
+export {sliderImgs, shopping_cart, correct, loader};
